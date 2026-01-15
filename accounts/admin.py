b700fb0b4a71586f328e112d15e0b2aac87daf8c
varshninagarajan29 <varshunagaraj2001@gmail.com>
2026-01-15 13:24:43 +1000

    SA-6: register OAuth models in Django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import OAuthClient, AuthorizationCode, OAuthToken
 
-# Register your models here.
+@admin.register(OAuthClient)
+class OAuthClientAdmin(admin.ModelAdmin):
+    list_display = ("name", "client_id", "is_active", "created_at")
+    readonly_fields = ("client_id", "created_at")
+
+@admin.register(AuthorizationCode)
+class AuthorizationCodeAdmin(admin.ModelAdmin):
+    list_display = ("user", "client", "expires", "is_used")
+    readonly_fields = ("code", "created_at")
+
+@admin.register(OAuthToken)
+class OAuthTokenAdmin(admin.ModelAdmin):
+    list_display = ("user", "client", "access_token_expires_at", "is_revoked")
+    readonly_fields = ("access_token", "refresh_token", "created_at")
+
