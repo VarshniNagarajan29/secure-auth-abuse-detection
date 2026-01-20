60e05560373df5698ab36d487e99d8595e6bd7ca
varshninagarajan29 <varshunagaraj2001@gmail.com>
2026-01-20 15:28:10 +1000

    SA-9: add authorization code validation and single-use logic

---
+++ accounts/services/authorization_code_service.py
@@ -0,0 +1,24 @@
+from django.utils import timezone
+from accounts.models import AuthorizationCode
+
+def validate_authorization_code(code, client):
+    try:
+        auth_code = AuthorizationCode.objects.get(code=code)
+    except AuthorizationCode.DoesNotExist:
+        return None
+
+    if auth_code.client != client:
+        return None
+
+    if auth_code.expires < timezone.now():
+        return None
+
+    if auth_code.is_used:
+        return None
+
+    return auth_code
+
+
+def mark_authorization_code_used(auth_code):
+    auth_code.is_used = True
+    auth_code.save(update_fields=['is_used'])
